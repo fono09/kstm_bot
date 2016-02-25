cd1edf294ba0717ceb8054a1fc99cc00b170d3f3
fono09 <fono@fono.jp>
2016-02-25 20:06:46 +0900

    Post to twitter with text from slack.

--- kstm-bot.rb
+++ kstm-bot.rb
@@ -14,6 +14,8 @@ end
 
 post '/' do
 	if params[:user_name] != settings['slack_name'] && params[:token]==settings['token'] then
+		slack_username = params[:user_name]
+		slack_text = params[:text]
 		ids = tc.follower_ids().to_a
 		cnt=(ids.size-1)/100+1
 		users=[]
@@ -24,7 +26,7 @@ post '/' do
 		end
 
 		users.each do |user|
-			tc.update("@#{user.screen_name} #{settings['reply_text']}")
+			tc.update("@#{user.screen_name} #{slack_username}「#{slack_text}」 #{settings['reply_text']}")
 		end
 
 		{'text'=> settings['slack_text'] }.to_json
